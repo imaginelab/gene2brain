20f0796fabef5a2914bcb972ada923003af477dd
Ulysses Popple <codes@ulysses.me>
2024-10-02 06:09:08 -0400

    correct dir for public

--- update-script.sh
+++ update-script.sh
@@ -8,7 +8,7 @@ systemctl --user daemon-reload
 cd frontend
 npm i
 npm run build
-cp public/* $HOME/Development/genes/backend/data/genes2brain_ulysses/
+cp public/* $HOME/Development/genes/backend/data/gene2brain_ulysses/
 cd ../backend
 npm i
 npm run build
